60e5c67785171faa2e1097020e0725cce792c279
shohjahon27 <xolpolatovshohjahon5@gmail.com>
2022-11-30 18:18:53 +0500

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const email = document.querySelector(".email"),
-  pass = document.querySelector(".password"),
-  form = document.querySelector("form");
-
-form.addEventListener( "click", (e) => {
-    // e.preventDefault();
-  function truOrFalse() {
-    if (email.value !== "admin" || pass.value !== "password") {
-      console.log("username or pass is wrong");
-    } else {
-        console.log("correct");
+const email = document.querySelector( ".email" ),
+  pass = document.querySelector( ".password" ),
+  form = document.querySelector( "form" );
+ 
+form.addEventListener( "submit", ( e ) => {
+  e.preventDefault();
+  if ( email.value !== "admin" || pass.value !== "pass123" ) {
+      alert("username or password incorrect")
+  } else {
+     location.href = "/admin.html"
     }
-  }
-    truOrFalse();
-    
-    e.target.reset()
-});
+  })
